5e9b653e3788a259eb97945f0592071be0d06396
Reuben <reuben@humanapi.co>
2020-02-26 20:39:37 -0800

    chore: Build

--- dist/lib/resource.js
+++ dist/lib/resource.js
@@ -1,35 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Operation;
-(function (Operation) {
-    Operation[Operation["FindOne"] = 0] = "FindOne";
-    Operation[Operation["FindMany"] = 1] = "FindMany";
-    Operation[Operation["Create"] = 2] = "Create";
-    Operation[Operation["Update"] = 3] = "Update";
-    Operation[Operation["Delete"] = 4] = "Delete";
-})(Operation || (Operation = {}));
 class Resource {
-    constructor(schema, permissions) {
-        this.transact = (transaction) => {
-            const grantAccess = transaction.onAccessGranted;
-            const denyAccess = transaction.onAccessDenied;
-            const { principal, role, strategy } = transaction;
-            /* Authorize the user by checking that they belong to at least one required group */
-            if (strategy === 'static') {
-                return principal.is(role) ? grantAccess() : denyAccess();
-            }
-            /* Authorize the actor by checking that they belong to all required groups */
-            if (strategy === 'exact') {
-                return principal.isExactly(role) ? grantAccess() : denyAccess();
-            }
-            /* TODO: Figure out a way to dynamically authorize the user without
-             * allowing the developer to write code with harmful side effects
-             * inside the resolution strategy */
-            /* At this point we know that the specified strategy is dynamic */
-            // const { requireTransactionResult } = role.resolvers[strategy];
-        };
-        this.schema = schema;
-        this.permissions = permissions;
+    constructor({ name, transactions }) {
+        this.name = name;
+        this.transactions = transactions;
+    }
+    getTransactionPlan({ name }) {
+        if (!this.transactions[name])
+            return null;
+        return this.transactions[name];
     }
 }
 exports.Resource = Resource;
--- dist/lib/transaction.js
+++ dist/lib/transaction.js
@@ -1,2 +1,36 @@
 "use strict";
-/* eslint-disable @typescript-eslint/no-explicit-any */
+Object.defineProperty(exports, "__esModule", { value: true });
+class RejectedTransactionError extends Error {
+    constructor() {
+        const message = 'RejectedTransactionError:'
+            + `User is not allowed to perform this transaction`;
+        super(message);
+        Error.captureStackTrace(this, RejectedTransactionError);
+    }
+}
+exports.RejectedTransactionError = RejectedTransactionError;
+class Transaction {
+    constructor(executor, options) {
+        const { principal, plan: { authorize, lazy } } = options;
+        /* If this transaction plan isn't lazy, then authorize right away */
+        if (!lazy) {
+            const isAllowed = authorize({ principal });
+            if (!isAllowed) {
+                return Promise.reject(new RejectedTransactionError());
+            }
+        }
+        /* Execute the main promise body, then authorize the actor based on the result */
+        const promise = new Promise(executor)
+            .then(resource => {
+            if (lazy) {
+                const isAllowed = authorize({ principal, resource });
+                if (!isAllowed) {
+                    return Promise.reject(new RejectedTransactionError());
+                }
+            }
+            return resource;
+        });
+        return promise;
+    }
+}
+exports.Transaction = Transaction;
